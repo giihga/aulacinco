90900ba10621bd3e34ac012cc6656d05eca4dba2
giihga <102439784+giihga@users.noreply.github.com>
2022-05-24 22:05:29 -0300

    Add files via upload

---
+++ aulacinco.py
@@ -0,0 +1,6 @@
+#2) Faça um programa que leia 4 notas de um aluno, salve as em uma lista e calcule a média das notas.#
+n1=float(input('Qual a primeira nota do aluno? '))
+n2=float(input('Qual a segunda nota do aluno? '))
+n3=float(input('Qual a terceira nota do aluno? '))
+n4=float(input('Qual a qurta nota do aluno? '))
+print('A média das notas do aluno é {}.'.format((n1+n2+n3+n4)/2))
